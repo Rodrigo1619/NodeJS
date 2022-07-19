28d5b5445e14f83f94cc6645fc0440274949a30b
rodrigo1619 <00067920@uca.edu.sv>
2022-07-18 18:01:59 -0600

    desestructuracion con arreglos agregada

--- 2-Refuerzo_JS/destructuracion.js
+++ 2-Refuerzo_JS/destructuracion.js
@@ -28,3 +28,21 @@ const {nombre, apellido, poder} = deadpool
 
 //console.log(nombre, apellido, poder)
 //imprimirHeroe(deadpool) 
+
+//*Desetruturacion con arreglos
+
+const heroes = ['Deadpool', 'Spiderman', 'Batman']
+//Manera "tradicional"
+/*const h1  = heroes[0]
+const h2 = heroes[1]
+const h3 = heroes[2]*/
+
+//haciendo uso de destructuracion
+//Lo de adentro del const solamente es el nombre que queremos darle
+const [h1, h2, h3] = heroes
+
+// Si queremos una manera para sacar solo a batman seria de esta manera
+const [ , , bat] = heroes
+
+console.log(h1, h2, h3)
+console.log(bat)
