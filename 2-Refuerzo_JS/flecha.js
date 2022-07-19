4f5282de6bf65309450cad27436cf03090a30263
rodrigo1619 <00067920@uca.edu.sv>
2022-07-18 21:50:38 -0600

    funcion flecha agregada

---
+++ 2-Refuerzo_JS/flecha.js
@@ -0,0 +1,14 @@
+// funciones tipicas
+function sumar(a,b){
+   return a+b
+}
+console.log(sumar(5, 10))
+
+//funciones flecha
+const suma = (a,b)=>{
+   return a+b
+}
+console.log( suma(1,2))
+//aunque si tenemos una funcion flecha de una sola linea podemos escribirla asi
+const suma1 = (a, b)=> a+b
+console.log(suma1(2,5))
