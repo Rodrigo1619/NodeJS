c781cf0462245c40dc821d8d6cb3941161e5a1c8
rodrigo1619 <00067920@uca.edu.sv>
2022-07-28 18:57:21 -0600

    models agregado con clases

--- 4-Tareas/app.js
+++ 4-Tareas/app.js
@@ -1,5 +1,7 @@
 import colors from 'colors';
 import {inquirerMenu, inquirerPausa} from './helpers/inquirer.js'; //se opta mejor por usar los import
+import Tarea from './models/tarea.js';
+import Tareas from './models/tareas.js';
 //const { mostrarMenu, pausa } = require('./helpers/mensajes'); esto era la version anterior
 
 
@@ -10,8 +12,12 @@ const main = async()=>{
     let opt = ''; //iniciamos la variable vacia
 
     do {
-        opt = await inquirerMenu() //le asignamos a la variable la opcion que vaya a meter el usuario 
-        console.log({opt})
+        //opt = await inquirerMenu() //le asignamos a la variable la opcion que vaya a meter el usuario 
+        //console.log({opt})
+        const tareas = new Tareas();
+        const tarea = new Tarea('Comprar comida');
+        tareas._listado[tarea.id] = tarea
+        console.log(tareas)
         if(opt !=='0') await inquirerPausa(); //para que el programa cierre de una sola vez
 
     } while (opt !== '0'); //se ejecutara media vez la opcion sea diferente de 0
