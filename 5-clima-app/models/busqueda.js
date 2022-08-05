615de16648bbb1b0231cf53d698bdcc04a713c20
rodrigo1619 <00067920@uca.edu.sv>
2022-08-04 19:20:24 -0600

    llamados a la api

--- 5-clima-app/models/busqueda.js
+++ 5-clima-app/models/busqueda.js
@@ -8,11 +8,17 @@ class Busqueda{
     }
 
     async ciudad(lugar=''){
-        //realizar la peticion http
-        //console.log('Ciudad de', lugar)
-        const resp = await axios.get('https://reqres.in/api/users?page=2')
-        console.log(resp.data)
-        return[]; //retornar lugares que sean iguales a los que el usuario metio
+
+        try {
+            //realizar la peticion http
+            //console.log('Ciudad de', lugar)
+            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Soyapango%2C%20San%20Salvador%2C%20El%20Salvador.json?proximity=ip&types=place%2Cpostcode%2Caddress&language=es&access_token=pk.eyJ1Ijoicm9kcmljazE2IiwiYSI6ImNsNmZveTdvaTAzMm8zanM1bGp2ZzhicDIifQ.5-JgXdqHa0J97W5Mb5yKrA')
+            console.log(resp.data)
+            return[]; //retornar lugares que sean iguales a los que el usuario metio
+            
+        } catch (error) {
+            return []
+        }
     }
 }
 export default Busqueda
