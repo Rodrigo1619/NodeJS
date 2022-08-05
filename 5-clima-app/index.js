52aa86717f58fee50195cc4a74677f52e407c0b6
rodrigo1619 <00067920@uca.edu.sv>
2022-08-04 22:05:43 -0600

    applicacion terminada

--- 5-clima-app/index.js
+++ 5-clima-app/index.js
@@ -16,15 +16,31 @@ const main = async()=>{
                 const lugares = await busquedas.ciudad(termino) //termino de busqueda
                 //Seleccionar el lugar
                 const idSeleccionado = await listarLugares(lugares);
+                if(idSeleccionado==='0') continue //haciendo que si el usuario pone cancelar no se muera la app
                 const lugarSel = lugares.find(l=> l.id === idSeleccionado)
+                //guardar en db
+                busquedas.agregarHistorial(lugarSel.nombre)
                 
+                //Clima
+                const clima = await busquedas.climaLugar(lugarSel.latitud, lugarSel.longitud)
+                console.log(clima)
+
+                //Mostrando resultados
+                console.clear();
                 console.log('\n Información de la ciudad\n'.green)
-                console.log('Ciudad:', lugarSel.nombre)
+                console.log('Ciudad:', lugarSel.nombre.yellow)
                 console.log('Latitud:', lugarSel.latitud)
                 console.log('Longitud:', lugarSel.longitud)
-                console.log('Temperatura:')
-                console.log('Minima:')
-                console.log('Maxima:')
+                console.log('Temperatura:', clima.temp)
+                console.log('Minima:', clima.min)
+                console.log('Maxima:', clima.max)
+                console.log('Como esta el clima:', clima.desc.yellow)
+
+                break;
+            case 2: busquedas.historialCapitalizado.forEach((lugar, i)=>{
+                const idx = `${i+1}.`.green
+                console.log(`${idx} ${lugar}`)
+            })
                 break;
         }
         console.log({opt})
--- 5-clima-app/models/busqueda.js
+++ 5-clima-app/models/busqueda.js
@@ -1,10 +1,21 @@
+import * as fs from 'fs'
 import axios from 'axios';
 
 class Busqueda{
-    historial = ['Soyapango', 'Madrid', 'San Jose']
+    historial = []
+    dbPath = './db/database.json'
 
     constructor(){
         //TODO: leer db si existe
+        this.leerDB()
+    }
+    get historialCapitalizado(){
+        return this.historial.map(lugar=>{
+            let palabra = lugar.split(' ')
+            palabra = palabra.map(p => p[0].toUpperCase() + p.substring(1));
+
+            return palabra.join(' ')
+        })
     }
 
     get paramsMapbox(){
@@ -12,6 +23,14 @@ class Busqueda{
             'access_token': process.env.MAPBOX_KEY, //literal se esta usando el token desde el .env
             'limit': 5,
             'language': 'es'
+        }
+    }
+    get paramsOpenWether(){
+        return{
+            appid: process.env.OPENWEATHER_KEY,
+            units: 'metric',
+            lang: 'es'
+
         }
     }
 
@@ -38,6 +57,54 @@ class Busqueda{
         } catch (error) {
             return []
         }
+    }
+
+    async climaLugar(lat, lon){
+        try {
+            const instance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+                params: {...this.paramsOpenWether, lat, lon} //desestructurando el getter
+            })
+            const resp = await instance.get();
+            const {weather, main} = resp.data
+
+            return{
+                desc: weather[0].description, //la description es un arreglo
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    agregarHistorial(lugar = ''){
+        if(this.historial.includes(lugar.toLocaleLowerCase())){
+            return; //solo retorna porque no tiene que grabar nada
+        }
+        //TODO: prevenir duplicidad
+        this.historial.unshift(lugar.toLocaleLowerCase())
+
+        //grabar en la db
+        this.guardarDB()
+    }
+    guardarDB(){
+        //esto es por si se tuvieran muchos archivos por guardar en la db
+        const payload={
+            historial: this.historial
+        }
+        fs.writeFileSync(this.dbPath, JSON.stringify(payload))
+    }
+    leerDB(){
+        //si el archivo no existe retornara un null
+        if(!fs.existsSync(this.dbPath)){
+            return
+        }
+        //pero si existe
+        const info = fs.readFileSync(this.dbPath, {encoding: 'utf-8'})
+        const data = JSON.parse(info)
+        this.historial = data.historial
     }
 }
 export default Busqueda
