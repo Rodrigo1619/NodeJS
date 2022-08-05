09f5ff96c5387c8104351b9f5cbdc014cd5b7293
rodrigo1619 <00067920@uca.edu.sv>
2022-08-04 19:47:28 -0600

    paramsMapbox e instancia de axios con url mas limpia

--- 5-clima-app/models/busqueda.js
+++ 5-clima-app/models/busqueda.js
@@ -7,12 +7,25 @@ class Busqueda{
         //TODO: leer db si existe
     }
 
+    get paramsMapbox(){
+        return {     
+            'access_token': 'pk.eyJ1Ijoicm9kcmljazE2IiwiYSI6ImNsNmZveTdvaTAzMm8zanM1bGp2ZzhicDIifQ.5-JgXdqHa0J97W5Mb5yKrA',
+            'limit': 5,
+            'language': 'es'
+        }
+    }
+
     async ciudad(lugar=''){
 
         try {
             //realizar la peticion http
-            //console.log('Ciudad de', lugar)
-            const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Soyapango%2C%20San%20Salvador%2C%20El%20Salvador.json?proximity=ip&types=place%2Cpostcode%2Caddress&language=es&access_token=pk.eyJ1Ijoicm9kcmljazE2IiwiYSI6ImNsNmZveTdvaTAzMm8zanM1bGp2ZzhicDIifQ.5-JgXdqHa0J97W5Mb5yKrA')
+            // asi se tenia antes de hacer cambios la url https://api.mapbox.com/geocoding/v5/mapbox.places/Madrid.json?types=place&language=es&access_token=YOUR_MAPBOX_ACCESS_TOKEN
+
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox
+            })
+            const resp = await instance.get();
             console.log(resp.data)
             return[]; //retornar lugares que sean iguales a los que el usuario metio
             
