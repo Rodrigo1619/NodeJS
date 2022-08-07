8a3aa471ab1ddf843b918635d076057958f0b2cd
rodrigo1619 <00067920@uca.edu.sv>
2022-08-07 16:19:48 -0600

    sirviendo una pagina web completa

--- 6-webserver/app.js
+++ 6-webserver/app.js
@@ -13,6 +13,13 @@ app.use(express.static('public'))
 app.get('/ola', (req, res)=>{
     res.send('camaron')
 })
+app.get('/generic',(req, res)=>{
+    res.sendFile(path.join(__dirname + '/public/generic.html'))
+})
+app.get('/elements',(req, res)=>{
+    res.sendFile(path.join(__dirname + '/public/elements.html'))
+})
+
 app.get('*', (req, res)=> {
     res.sendFile(path.join(__dirname + '/public/404.html'))
 })
