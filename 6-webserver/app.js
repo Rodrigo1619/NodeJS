5f4965a96ada898e6d7f8f45adc8beeb3e1da1bb
rodrigo1619 <00067920@uca.edu.sv>
2022-08-07 16:51:08 -0600

    renderisado con hbs

--- 6-webserver/app.js
+++ 6-webserver/app.js
@@ -4,14 +4,19 @@ const app = express()
 const port = 8080
 const __dirname = path.resolve()
 
+app.set('view engine', 'hbs')
+
 // Servir contenido estatico
 app.use(express.static('public'))
 
 /*app.get('/', (req, res)=> {
     res.send('ola camaron')
 })*/
-app.get('/ola', (req, res)=>{
+/*app.get('/ola', (req, res)=>{
     res.send('camaron')
+})*/
+app.get('/',(req, res)=>{
+    res.render('home')
 })
 app.get('/generic',(req, res)=>{
     res.sendFile(path.join(__dirname + '/public/generic.html'))
@@ -21,7 +26,7 @@ app.get('/elements',(req, res)=>{
 })
 
 app.get('*', (req, res)=> {
-    res.sendFile(path.join(__dirname + '/public/404.html'))
+    res.sendFile(path.join(__dirname + '/public/back/404.html'))
 })
 app.listen(port, ()=>{
     console.log(`Corriendo en ${port}`)
