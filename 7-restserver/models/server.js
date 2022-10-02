7e5f00239d6cebb5055cd4e1f39d9b8fc0a3d063
rodrigo1619 <00067920@uca.edu.sv>
2022-10-02 16:21:47 -0600

    feat: se agregaron los endpoints en routes

--- 7-restserver/models/server.js
+++ 7-restserver/models/server.js
@@ -23,7 +23,30 @@ class Server{
         //el app.get no existe, pero tenemos acceso al this.app.get 
         //debemos cambiar el ('/') porque el middleware ya lo esta usando
         this.app.get('/camaron', (req,res)=>{
-            res.send('Ola camaron sin cola');
+            //res.send('Ola camaron sin cola'); cambiamos el send por json para no mandar un html sino un archivo en formato json se hace la peticion en postman con la url
+            res.json({
+                msg: 'get API'
+            });
+        });
+        this.app.put('/camaron', (req,res)=>{  
+            res.json({
+                msg: 'put API'
+            });
+        });
+        this.app.post('/camaron', (req,res)=>{
+            res.json({
+                msg: 'post API'
+            });
+        });
+        this.app.delete('/camaron', (req,res)=>{
+            res.json({
+                msg: 'delete API'
+            });
+        });
+        this.app.patch('/camaron', (req,res)=>{
+            res.json({
+                msg: 'patch API'
+            });
         });
     }
 
